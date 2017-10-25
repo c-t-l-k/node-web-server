f7e4175724dc62bb6186395be4ccb33b8f695a83
cyruslognonne <cyrus7@hotmail.fr>
2017-10-25 13:09:19 -0400

    Cleaning code

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const express = require("express");
 var app = express();
 
-// This will let us set up an handler for an HTTP get request.
 app.get("/", (req, res) => {
-  // Here, there gonna get this back as the body data
-  // res.send("<h1>Hello Express!</h1>");
-
-  // Now the content-type of the reponse header has changed to json
   res.send({
     name: "Cyrus",
     likes: ["Mélo", "Louisie"]
@@ -14,7 +9,6 @@ app.get("/", (req, res) => {
 });
 
 
-// Create a new route for about
 app.get("/about", (req, res) => {
   res.send("<h1>About page</h1>");
 });
@@ -26,5 +20,4 @@ app.get("/bad", (req, res) => {
   });
 });
 
-// This is gonna bind the app on a port of our machine
 app.listen(3000);
