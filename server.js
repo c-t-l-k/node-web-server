fbb1e4277c158a608979d36c3115ff1634ba98da
cyruslognonne <cyrus7@hotmail.fr>
2017-10-25 17:07:19 -0400

    =Changing the env. port p.2

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const hbs = require("hbs");
 const fs = require("fs");
 
+// Here, the app is configured to run with Heroku of locally on 300
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -63,6 +65,6 @@ app.get("/bad", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
