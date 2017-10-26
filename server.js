c3e8add585a3c511d15e75a1d6e328fb11c0dfac
cyruslognonne <cyrus7@hotmail.fr>
2017-10-26 11:52:47 -0400

    Adding project page

--- server.js
+++ server.js
@@ -58,6 +58,12 @@ app.get("/about", (req, res) => {
   });
 });
 
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    pageTitle: "project page"
+  });
+});
+
 
 app.get("/bad", (req, res) => {
   res.send({
